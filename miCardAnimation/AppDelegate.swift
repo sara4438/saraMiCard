4aeaa7c37324b5a2e77473cf2d5ff10ab226dd2e
sara_yang <sara_yang@mail.chungyo.net>
2021-01-12 14:41:05 +0800

    [ADD] fb, google登入ok

--- miCardAnimation/AppDelegate.swift
+++ miCardAnimation/AppDelegate.swift
@@ -13,8 +13,37 @@ import GoogleSignIn
 import LineSDK
 
 @UIApplicationMain
-    class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate , GIDSignInDelegate {
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        if let error = error {
+            if (error as NSError).code == GIDSignInErrorCode.hasNoAuthInKeychain.rawValue {
+                print("The user has not signed in before or they have since signed out.")
+            } else {
+                print("ccccc","\(error.localizedDescription)")
+            }
+            return
+        }
+        // Perform any operations on signed in user here.
+//        let userId = user.userID                  // For client-side use only!
+//        let idToken = user.authentication.idToken // Safe to send to the server
+//        let fullName = user.profile.name
+//        let givenName = user.profile.givenName
+//        let familyName = user.profile.familyName
+//        let email = user.profile.email
+        
+    // ...
+}
+
         func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+            // Override point for customization after application launch.
+            FirebaseApp.configure() //建立firebase
+            ///for Google Login
+            GIDSignIn.sharedInstance()?.clientID = "412741437040-i4n6n6qqhl7gvbdq16fg6un2anifvu7h.apps.googleusercontent.com"
+            GIDSignIn.sharedInstance()?.delegate = self
+                // If user already sign in, restore sign-in state.
+            GIDSignIn.sharedInstance()?.restorePreviousSignIn()
+            ///for Facebook Login
+            ApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions: launchOptions)
             return true
         }
 
@@ -31,6 +60,12 @@ import LineSDK
             }
             
             return result
+        }
+        
+        func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!,
+                  withError error: Error!) {
+            // Perform any operations when the user disconnects from app here.
+            // ...
         }
 
         func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
--- miCardAnimation/BViewController.swift
+++ miCardAnimation/BViewController.swift
@@ -13,10 +13,45 @@ import RxSwift
 class BViewController : UIViewController{
     @IBOutlet weak var toThrottle: UIButton!
     @IBOutlet weak var toQueue: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
     override func viewDidLoad() {
-       super.viewDidLoad()
+        super.viewDidLoad()
+        if let currentUser = Auth.auth().currentUser {
+            self.nameLabel.text = currentUser.displayName
+            self.emailLabel.text = currentUser.email
+        }
     }
     
+    @IBAction func logout(_ sender: Any) {
+        let firebaseAuth = Auth.auth()
+        do {
+            try firebaseAuth.signOut()
+            let alertController = UIAlertController(title: "Message", message: "Log out successfully" , preferredStyle: .alert)
+            let okayAction = UIAlertAction(title: "好的", style: .cancel, handler: { _ in
+                if let viewController = self.storyboard?.instantiateViewController(withIdentifier: "firstPage") {
+                    UIApplication.shared.keyWindow?.rootViewController = viewController
+                    self.dismiss(animated: true, completion: nil)
+                }
+            })
+            alertController.addAction(okayAction)
+            self.present(alertController, animated: true, completion: nil)
+
+
+
+        } catch let signOutError as NSError {
+            print ("Error signing out: %@", signOutError)
+        }
+    }
+
+
+    //  let okayAction = UIAlertAction(title: "OK", style: .cancel, handler: { _ in
+    //self.hiLabel.isHidden = true
+    //self.nameLabel.text = "Please login."
+    //try firebaseAuth.signOut()
+    //})
+
+
     @IBAction func toHorizontal(_ sender: Any) {
         UIState.instance.isVertical.onNext(false)
     }
